c208f9875337262fe600d1eed64608eecafb0a65
Ruslan Gustomiasov <rusik@4ege.ru>
2019-08-20 23:18:34 +0200

    commit

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+./helm.sh
+./istio.sh
+./gitlab.sh
--- istio.sh
+++ istio.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 set -x
-kubectl label namespace default istio-injection=enabled
 
+#curl -L https://git.io/getLatestIstio | ISTIO_VERSION=1.2.4 sh -
+#export PATH="$PATH:./istio-1.2.4/bin"
+#kubectl create ns istio-system
+#kubectl apply -f kiali-secret.yaml -n istio-system
+#kubectl label namespace default istio-injection=enabled
+#helm install istio-1.2.4/install/kubernetes/helm/istio-init --name istio-init --namespace istio-system
+#sleep 60
+kubectl create -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
+helm install istio-1.2.4/install/kubernetes/helm/istio --name=istio --namespace=istio-system --set gateways.enabled=true --set gateways.istio-ingressgateway.enabled=true --set grafana.enabled=true --set grafana.persist=true --set certmanager.enabled=true --set kiali.enabled=true --set grafana.accessMode=ReadWriteOnce
+kubectl apply -f kiali-gateway.yaml -n istio-system
